cbf749383e8e59f8e5c476d7dda3832ae5f9d157
paiain <86291726+paiain@users.noreply.github.com>
2021-08-21 00:30:28 +0700

    modifiy

--- webapp/settings.py
+++ webapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-x@$()s_h^cq4y46wp&9h%+ewh@+9yktrr(s-==vof&kd5@24d&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1','paisindjnew.herokuapp.com',]
 
 
 # Application definition
