7725253ca5befb5f7d3284cf807d9aec31dfb4df
paiain <86291726+paiain@users.noreply.github.com>
2021-08-21 15:11:15 +0700

    repar to deploy

--- webapp/local_settings.py
+++ webapp/local_settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,9 +90,6 @@ DATABASES = {
     }
 }
 
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
